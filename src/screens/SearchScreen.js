ded33fdff30956d42c6ac62cbc34bb1969e44a3a
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-07 16:33:08 -0700

    add search screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+  return <Text>Search Screen Baby</Text>;
+};
+export default SearchScreen;

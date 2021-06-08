0cea2c276b024fd3431d9f905c884b26c0ebfdcb
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-07 17:48:22 -0700

    show searchbar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-
+import SearchBar from "../components/SearchBar";
 const SearchScreen = () => {
-  return <Text>Search Screen Baby</Text>;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <View>
+      <SearchBar
+        term={searchTerm}
+        setSearchTerm={(newTerm) => setSearchTerm(newTerm)}
+        onTermSubmit={() => console.log("term submitted")}
+      />
+      <Text>Term is: {searchTerm}</Text>
+    </View>
+  );
 };
 export default SearchScreen;

743156554acc91471d46d5475aa084e0b7ebfad5
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-08 09:39:51 -0700

    create api call on search term submit

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,36 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 const SearchScreen = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "santa rosa",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMsg("Something went wrong");
+    }
+  };
 
   return (
     <View>
       <SearchBar
         term={searchTerm}
         setSearchTerm={(newTerm) => setSearchTerm(newTerm)}
-        onTermSubmit={() => console.log("term submitted")}
+        onTermSubmit={() => searchApi()}
       />
-      <Text>Term is: {searchTerm}</Text>
+      {errorMsg ? <Text>{errorMsg}</Text> : null}
+      <Text>We have found this many results: {results.length}</Text>
     </View>
   );
 };

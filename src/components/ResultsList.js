c450bbfbe9dc6713104582e61e4ea4330e02c8cf
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-08 14:42:39 -0700

    wrap results in touchable opacity

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Text, View, StyleSheet, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import ResultsDetail from "./ResultsDetail";
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
   return (
     <View>
       <Text style={styles.textStyle}>{title}</Text>
@@ -12,7 +18,11 @@ const ResultsList = ({ title, results }) => {
         data={results}
         keyExtractor={(result) => result.id}
         renderItem={({ item }) => {
-          return <ResultsDetail result={item} />;
+          return (
+            <TouchableOpacity onPress={() => navigation.navigate("Results")}>
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          );
         }}
       />
     </View>
